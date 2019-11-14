7055cf76072c173b6fcc5af1660940ec64856be6
Yashwanth Nannapaneni <yashwanth620@gmail.com>
2019-11-14 14:10:34 -0500

    Changing test to insert more numbers

--- BTreeOLC/BTreeTest.cpp
+++ BTreeOLC/BTreeTest.cpp
@@ -2,19 +2,28 @@
 #include "BTree_single_threaded.h"
 #include "BTree_rtm.h"
 #include <cassert>
+#include <vector>
+#include <time.h>
 
+#define NUM_ELEMENTS 500
 
 int main() {
     btreertm::BTree<int64_t, int64_t> idx;
-    int64_t keys[] = {10, 20 , 30, 40, 50, 60};
-    int64_t values[] = {100, 200 , 300, 400, 500, 600};
 
-    for(auto i = 0; i < 6; i++){
+    std::vector<int64_t> keys;
+    std::vector<int64_t> values;
+
+    for(int i = 0; i < NUM_ELEMENTS; i++) {
+        keys.push_back(rand() % 10000);
+        values.push_back(rand() % 10000);
+    }
+
+    for(auto i = 0; i < NUM_ELEMENTS; i++){
         idx.insert(keys[i], values[i]);
     }
 
     assert(idx.checkTree());
-    for(int i = 0; i < 6; i++){
+    for(int i = 0; i < NUM_ELEMENTS; i++){
         int64_t result;
         assert(idx.lookup(keys[i], result));
         assert(result == values[i]);

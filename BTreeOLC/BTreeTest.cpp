c94e7a495b70b36713361948fac5d0293e8ee5e7
Yashwanth Nannapaneni <yashwanth620@gmail.com>
2019-11-14 15:01:08 -0500

    For test generating unique keys

--- BTreeOLC/BTreeTest.cpp
+++ BTreeOLC/BTreeTest.cpp
@@ -4,18 +4,28 @@
 #include <cassert>
 #include <vector>
 #include <time.h>
+#include <iostream>
+#include <algorithm>
 
 #define NUM_ELEMENTS 500
 
 int main() {
     btreeolc::BTree<int64_t, int64_t> idx;
 
+    srand(time(NULL));
     std::vector<int64_t> keys;
     std::vector<int64_t> values;
 
     for(int i = 0; i < NUM_ELEMENTS; i++) {
-        keys.push_back(rand() % 10000);
-        values.push_back(rand() % 10000);
+        int64_t randKey = rand() % 30000;
+        while(std::find(keys.begin(), keys.end(), randKey) != keys.end()) {
+            randKey = rand() % 30000;
+        }
+
+        keys.push_back(randKey);
+        values.push_back(rand() % 30000);
+        //keys.push_back(i);
+        //values.push_back(i * 10);
     }
 
     for(auto i = 0; i < NUM_ELEMENTS; i++){
@@ -26,6 +36,10 @@ int main() {
     for(int i = 0; i < NUM_ELEMENTS; i++){
         int64_t result;
         assert(idx.lookup(keys[i], result));
+        if(result != values[i]) {
+            printf("Looking up: %lld \n", keys[i]);
+            printf("Result %lld, value %lld \n", result, values[i]);
+        }
         assert(result == values[i]);
     }
 }
